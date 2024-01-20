ffc7f280598e37219cd32bd86c8d43b46ebfcf3e
Topallaj Denis <denis.topallaj@student.howest.be>
2024-01-20 01:16:26 +0100

    add: new version in the server that takes in a page link from rumble and multiple clip timestamps and then returns a zip folder of the clips with subtitles. modified: basic files have been modified for personal purposes client hasn't been modified based on the recent server changes yet

---
+++ brain-rot-tok-server/generate_rumble_clips.py
@@ -0,0 +1,94 @@
+import os
+import shutil
+from create_subtitles import hex_to_ffmpeg_color, create_subtitles
+from transcribe_audio import transcribe_audio
+from video_to_audio import extract_audio
+import subprocess
+
+
+def generate_rumble_clips(customization_options, output_directory="./data/rumble/clips"):
+	customization_options['font_color'] = hex_to_ffmpeg_color(customization_options['font_color'])
+	audio_file_path = "./data/rumble/audio.mp3"
+	subtitle_file = "./data/rumble/subtitles.srt"
+
+	clips = []
+
+	for idx, clip in enumerate(customization_options['clips']):
+		# Generate the clip
+		video_clip_path = cut_video(customization_options['video_url'], clip['start'], clip['end'], output_directory, idx)
+		clip['clip_link'] = video_clip_path
+		clips.append(clip)
+
+		# Generate the subtitles
+		extract_audio(video_clip_path, audio_file_path)
+		transcription = transcribe_audio(audio_file_path)
+		create_subtitles(transcription, subtitle_file)
+
+		# Add the subtitle to the video
+		subtitle_video_clip = add_subtitle(video_clip_path, subtitle_file, customization_options)
+
+		# Add the text to the video
+		clip['clip_link'] = add_text_to_video(subtitle_video_clip, customization_options)
+	
+	return clips
+
+
+def cut_video(input_url, start_time, end_time, output_directory, idx):
+	output_filepath = os.path.join(output_directory, f'output_clip_{idx}.mp4')
+
+	command = [
+		'ffmpeg',
+		'-i', input_url,
+		'-ss', start_time,
+		'-to', end_time,
+		'-c', 'copy',
+		'-y', output_filepath
+	]
+
+	subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	return output_filepath
+
+
+def add_subtitle(video_path, subtitle_file, customization_options):
+	output_filepath = video_path.replace('.mp4', '_with_subtitle.mp4')
+
+	subtitle_cmd = [
+		"ffmpeg",
+		"-i", video_path,
+		"-vf", f"subtitles={subtitle_file}:force_style='Fontsize={customization_options['font_size']},PrimaryColour={customization_options['font_color']},Fontname={customization_options['font_family']},MarginV=50'",
+		"-c:a", "copy",
+		'-y', output_filepath
+	]
+
+	subprocess.run(subtitle_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	return output_filepath
+
+
+def add_text_to_video(video_path, customization_options):
+	output_filepath = video_path.replace('.mp4', '_with_text.mp4')
+
+	text_cmd = [
+		"ffmpeg",
+		"-i", video_path,
+		"-vf", f"drawtext=fontfile={customization_options['font_family']}:text='{customization_options['credit']}':fontcolor=white:fontsize={customization_options['credit_size']}:x=(w-text_w)/2:y=(h-text_h)-20",
+		"-c:a", "copy",
+		'-y', output_filepath
+	]
+
+	subprocess.run(text_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	return output_filepath
+
+def remove_clips_dir():
+	folder = './data/rumble/clips'
+	for filename in os.listdir(folder):
+		file_path = os.path.join(folder, filename)
+		try:
+			if os.path.isfile(file_path) or os.path.islink(file_path):
+				os.unlink(file_path)
+			elif os.path.isdir(file_path):
+				shutil.rmtree(file_path)
+		except Exception as e:
+			print('Failed to delete %s. Reason: %s' % (file_path, e))
--- brain-rot-tok-server/server.py
+++ brain-rot-tok-server/server.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile, Form, Body
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.exceptions import HTTPException
 import shutil
 import uvicorn
 from generate_result import generate_subtitles, generate_combined_video
 from generate_minecraft_subtitles import generate_minecraft_subtitles
 from generate_basic_subtitles import generate_basic_subtitles
+from generate_rumble_clips import generate_rumble_clips, remove_clips_dir
 import os
+import io
+import zipfile
 
 app = FastAPI()
 
@@ -88,7 +92,7 @@ async def create_minecraft_video(
 
 
 @app.post("/basic-generate-subtitles")
-async def create_minecraft_video(
+async def create_basic_video(
 	video: UploadFile = File(...),
 	color: str = Form(...),
 	size: int = Form(...),
@@ -117,6 +121,53 @@ async def create_minecraft_video(
 		return {"message": "Error generating subtitles!"}
 
 	return FileResponse(result_path, media_type="video/mp4", filename="result.mp4")
+
+
+@app.post("/rumble-generate-subtitles")
+async def create_rumble_video(request_data: dict = Body(...)):
+
+	# Extract values from the request_data dictionary
+	video_url = request_data.get("video_url", "")
+	color = request_data.get("color", "")
+	size = request_data.get("size", 0)
+	font = request_data.get("font", "")
+	clips = request_data.get("clips", [])
+
+	options = {
+		"video_url": video_url,
+		"clips": clips,
+		"font_color": color,
+		"font_size": size,
+		"font_family": font,
+		"border_size": 1,
+		"credit": "Made with BrainRotTok",
+		"credit_size": 15,
+	}
+
+	try:
+		clips = generate_rumble_clips(options)
+		
+		# Create a zip archive in memory
+		zip_buffer = io.BytesIO()
+		with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+			for clip in clips:
+				clip_name = clip["name"]
+				clip_link = clip["clip_link"]
+				zip_file.write(clip_link, arcname=f"clips/{clip_name}.mp4")
+
+		# Reset the buffer position to the beginning
+		zip_buffer.seek(0)		
+
+		# remove the clips directory content
+		remove_clips_dir()
+
+		# Return the zip archive as a StreamingResponse
+		return StreamingResponse(io.BytesIO(zip_buffer.read()), media_type="application/zip", headers={
+			'Content-Disposition': 'attachment; filename="clips.zip"'
+		})
+	except Exception as e:
+		# Handle exceptions appropriately
+		raise HTTPException(status_code=500, detail=str(e))
 
 
 if __name__ == "__main__":
