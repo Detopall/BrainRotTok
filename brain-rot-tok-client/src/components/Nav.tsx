95c17a8578e5afb6edd48c4bc9762c6ed7edcb61
Topallaj Denis <denis.topallaj@student.howest.be>
2024-02-09 18:18:46 +0100

    modified the nav by adding icons to each link. also added the icons to the folder

--- brain-rot-tok-client/src/components/Nav.tsx
+++ brain-rot-tok-client/src/components/Nav.tsx
@@ -1,11 +1,16 @@
 import { Link, useLocation } from 'react-router-dom';
+import basicIcon from '../assets/svg/basic.svg';
+import videoCutterIcon from '../assets/svg/video_cutter.svg';
+import subwaySurfersIcon from '../assets/svg/subway_surfers.svg';
+import rumbleIcon from '../assets/svg/rumble.svg';
+import minecraftIcon from '../assets/svg/minecraft.svg';
 
 const navLinks = [
-	{ path: '/', text: 'Basic Video Type' },
-	{ path: '/subway-surfers-type', text: 'Subway Surfers Type' },
-	{ path: '/minecraft-reddit-type', text: 'Minecraft-Reddit Type' },
-	{ path: '/rumble-video-type', text: 'Rumble Video Type' },
-	{ path: '/video-cutter', text: 'Video Cutter' },
+	{ path: '/', text: 'Basic Video Type', icon: basicIcon },
+	{ path: '/subway-surfers-type', text: 'Subway Surfers Type', icon: subwaySurfersIcon },
+	{ path: '/minecraft-reddit-type', text: 'Minecraft-Reddit Type', icon: minecraftIcon },
+	{ path: '/rumble-video-type', text: 'Rumble Video Type', icon: rumbleIcon },
+	{ path: '/video-cutter', text: 'Video Cutter', icon: videoCutterIcon },
 ];
 
 function Nav() {
@@ -17,6 +22,7 @@ function Nav() {
 				<ul>
 					{navLinks.map((link) => (
 						<li key={link.path}>
+							<img src={link.icon} alt={link.text} className="nav-icon" />
 							<Link to={link.path} className={location.pathname === link.path ? 'nav-link active' : 'nav-link'}>
 								{link.text}
 							</Link>
